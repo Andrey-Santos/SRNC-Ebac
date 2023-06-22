b3f173ffb92b6c7eb6aa28e0cc9172a855306217
Andrey C. Dos Santos <Andrey-Carlos@outlook.com>
2023-06-22 16:21:42 -0300

    Adicionado validações de processamento e comentários

--- Cartorio.c
+++ Cartorio.c
@@ -4,13 +4,46 @@
 
 int main()
 {
-	setlocale(LC_ALL, "Portuguese");
+	int opcao = 0;//Definindo variavéis
+	
+	setlocale(LC_ALL, "Portuguese");//Definido Linguagem
+	
+	createMenu();//Criação do menu
+	
+	scanf("%d", &opcao);//Armazenando a escolha do usuário
+	
+	system("cls");
+		
+		
+	if(opcao==1)//Inicio da seleção
+	{
+		printf("Você escolheu o registro de nomes!");
+	}
+		
+	if(opcao==2)
+	{
+		printf("Você escolheu o consultar os nomes!");
+	}
+		
+	if(opcao==3)
+	{
+		printf("Você escolheu deletar nomes!");
+	}	
+	
+	if(opcao>=4 || opcao<=0)
+	{
+		printf("Está opção não está disponível");
+	}//Fim seleção
+}
+
+void createMenu()
+{
 	printf("### Cartório da EBAC ### \n\n");
 	printf("Escolha uma das opçãos do menu: \n\n");		
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n\n");
 	
-	printf("Esse Software foi desenvolvido e testado por Andrey C. Dos Santos, baseado no curso TI do Zero Ao Pro da EBAC\n");
+	printf("Opção:");
 }
 
